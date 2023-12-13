ab96138b0f5e64dad9356c42ae935264f353b5e3
Endorvey <plusheviy06@gmail.com>
2023-12-13 03:18:26 +0300

    add labs

---
+++ MO/trunk/Леонович_Александр_АС-61/Lab_4/app/src/main/java/com/example/notes42/MyNote.java
@@ -0,0 +1,11 @@
+package com.example.notes42;
+
+public class MyNote {
+    String name;
+    String description;
+    public MyNote(String name,String description)
+    {
+        this.name = name;
+        this.description = description;
+    }
+}
